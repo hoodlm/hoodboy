60ebbc56816c6bce3301b5cd401ddd185270d81a
Logan Hood <hoodlma@gmail.com>
2020-12-31 14:31:17 -0500

    setup kt project

---
+++ src/test/kotlin/TestTest.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TestTest {
+    @Test fun testTest() {
+        assertEquals(0, 0)
+    }
+
+    @Test fun testFail() {
+        assertEquals(0, 5)
+    }
+}
