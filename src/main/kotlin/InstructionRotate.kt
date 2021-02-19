5281fa5df5094bb8e1a1e425d633bb967eba5d81
Logan Hood <hoodlma@gmail.com>
2021-02-19 16:18:24 -0500

    Attempt at refactoring a base rotate instruction

--- src/main/kotlin/InstructionRotate.kt
+++ src/main/kotlin/InstructionRotate.kt
@@ -1,35 +1,102 @@
-interface InstructionRotateLeft: Instruction {
+interface InstructionRotateBase: Instruction {
     override val size: UShort
         get() = 2u
 
     fun getValue(r: Registers): UByte
     fun setValue(r: Registers, v: UByte)
 
+
     override fun invoke(r: Registers, m: Memory, data: List<UByte>) {
         val currentValue = getValue(r)
         // Stash the current Carry flag value; we'll use this for bit 0 later:
         val previousCarryFlag = r.flagC()
-        // Carry flag will contain the old value of bit 7:
-        r.setFlagC(BYTE_VALUE_ZERO != currentValue.and(0b1000_0000u))
+        setCarryFlag(currentValue, r)
+        val rotated = doRotation(currentValue)
+        val finalValue = handlePreviousCarryFlag(rotated, previousCarryFlag)
+        setValue(r, finalValue)
+        setFinalFlags(finalValue, r)
+    }
 
+    /**
+     * If this rotate instruction should update the carry flag, implement that logic here.
+     * For example, the standard RL instruction copies the input value's bit 7 into the carry flag.
+     */
+    fun setCarryFlag(input: UByte, r: Registers) {
+        // NOOP by default
+    }
+
+    /**
+     * After setCarryFlag but before handlePreviousCarryFlag, perform the actual R or L rotation.
+     */
+    fun doRotation(input: UByte): UByte
+
+    /**
+     * If the previous value of the Carry flag affects the output of this instruction,
+     * implement that logic here. For example, for standard RL instructions, the previous
+     * carry flag value is copied into bit 0.
+     */
+    fun handlePreviousCarryFlag(value: UByte, previousFlag: Boolean): UByte {
+        return value // NOOP by default
+    }
+
+    /**
+     * After rotation is done, set H, N, Z flags.
+     */
+    fun setFinalFlags(finalValue: UByte, r: Registers) {
+        // NOOP by default
+    }
+}
+
+// Base implementation for RL and RR instructions
+interface InstructionRotateLeftBase: InstructionRotateBase {
+    override fun doRotation(input: UByte): UByte {
         // Clear bit 7:
-        val preRotate = currentValue.and(0b0111_1111u)
+        val preRotate = input.and(0b0111_1111u)
         // Assertion is to make sure we don't overflow:
         assert(preRotate < 0b1000_0000u)
         // Then a leftshift is just a multiplication by 2.
         val rotated = preRotate * 2u
-        val finalValue =
-            if (previousCarryFlag) {
-                // "The previous value of the carry flag are copied to bit 0 of the register"
-                rotated.xor(0b0000_0001u)
-            } else {
-                rotated
-            }
         // Again, an assertion, to make sure there was no overflow:
-        assert(finalValue <= 0xFFu)
-        setValue(r, finalValue.toUByte())
+        assert(rotated <= 0xFFu)
+        return rotated.toUByte()
+    }
+}
 
-        if (0u == finalValue) {
+interface InstructionRotateLeft: InstructionRotateLeftBase {
+    override fun handlePreviousCarryFlag(value: UByte, previousFlag: Boolean): UByte {
+        return if (previousFlag) {
+            // "The previous value of the carry flag are copied to bit 0 of the register"
+            value.xor(0b0000_0001u)
+        } else {
+            value
+        }
+    }
+
+    /**
+     * Pass the current register value; will set carry flag depending on instruction type.
+     */
+    override fun setCarryFlag(input: UByte, r: Registers) {
+        // Carry flag will contain the old value of bit 7:
+        r.setFlagC(BYTE_VALUE_ZERO != input.and(0b1000_0000u))
+    }
+
+    override fun doRotation(input: UByte): UByte {
+        // Clear bit 7:
+        val preRotate = input.and(0b0111_1111u)
+        // Assertion is to make sure we don't overflow:
+        assert(preRotate < 0b1000_0000u)
+        // Then a leftshift is just a multiplication by 2.
+        val rotated = preRotate * 2u
+        // Again, an assertion, to make sure there was no overflow:
+        assert(rotated <= 0xFFu)
+        return rotated.toUByte()
+    }
+
+    /**
+     * Set N/Z/H flags depending on instruction type.
+     */
+    override fun setFinalFlags(result: UByte, r: Registers) {
+        if (BYTE_VALUE_ZERO == result) {
             r.setFlagZ(true)
         } else {
             r.setFlagZ(false)
@@ -37,6 +104,7 @@ interface InstructionRotateLeft: Instruction {
         r.setFlagN(false)
         r.setFlagH(false)
     }
+
 }
 
 class InstructionRotateLeftA: InstructionRotateLeft {
